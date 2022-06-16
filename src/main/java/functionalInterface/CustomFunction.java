00d44c3f6e252550c6e3268dda8b1f3a52adcfb7
Deepak Palanisamy <deepak@breezeware.net>
2022-06-16 17:05:18 +0530

    multi-function execution using BiFunction

--- src/main/java/functionalInterface/CustomFunction.java
+++ src/main/java/functionalInterface/CustomFunction.java
@@ -1,5 +1,6 @@
 package functionalInterface;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class CustomFunction {
@@ -13,14 +14,19 @@ public class CustomFunction {
         int multipliedNumber = multiplyByTenUsingFunctionalInterface.apply(incrementedNumber);
         System.out.printf("Using Functional Interface Method. Multiplied number is %d\n", multipliedNumber);
 
-        int incrementedAndMultipliedNumber = incrementBy1AndMultiplyBy10.apply(0);
-        System.out.printf("Using Functional Interface Method. Incremented and Multiplied number is %d", incrementedAndMultipliedNumber);
+        int incrementedAndMultipliedNumber = incrementBy1AndMultiplyBy10UsingFunctionalInterface.apply(0);
+        System.out.printf("Using Functional Interface Method. Incremented and Multiplied number is %d\n", incrementedAndMultipliedNumber);
+
+        int incrementedAndMultipliedNumberByBiFunction = incrementBy1AndMultiplyBy10UsingBiFunction.apply(1, 20);
+        System.out.printf("Using BiFunction Functional Interface Method. Incremented and Multiplied number is %d", incrementedAndMultipliedNumberByBiFunction);
 
     }
 
     static Function<Integer, Integer> incrementByOneUsingFunctionalInterface = (number) -> number + 1;
     static Function<Integer, Integer> multiplyByTenUsingFunctionalInterface = (number) -> number * 10;
-    static Function<Integer, Integer> incrementBy1AndMultiplyBy10 = incrementByOneUsingFunctionalInterface.andThen(multiplyByTenUsingFunctionalInterface);
+    static Function<Integer, Integer> incrementBy1AndMultiplyBy10UsingFunctionalInterface = incrementByOneUsingFunctionalInterface.andThen(multiplyByTenUsingFunctionalInterface);
+
+    static BiFunction<Integer, Integer, Integer> incrementBy1AndMultiplyBy10UsingBiFunction = (numberToBeIncremented, numberToBeMultiplied) -> (numberToBeIncremented + 1) * numberToBeMultiplied;
 
     static int incrementByOneUsingConventionalMethod(int number) {
         return number + 1;
