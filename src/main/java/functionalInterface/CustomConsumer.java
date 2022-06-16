46846468e7c40bf5f7800c7d879259698c980a09
Deepak Palanisamy <deepak@breezeware.net>
2022-06-16 17:15:40 +0530

    Consumer FunctionalInterface implementation

--- src/main/java/functionalInterface/CustomConsumer.java
+++ src/main/java/functionalInterface/CustomConsumer.java
@@ -3,15 +3,21 @@ package functionalInterface;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.function.Consumer;
+
 public class CustomConsumer {
 
     public static void main(String[] args) {
         greet(new Customer("Deepak", "+91 9489954765"));
+        greetUsingConsumerFI.accept(new Customer("Naresh", "+91 8489308909"));
     }
 
     static void greet(Customer customer) {
-        System.out.printf("Hi %s, your phone number is %s", customer.getName(), customer.getPhoneNumber());
+        System.out.printf("Hi %s, your phone number is %s\n", customer.getName(), customer.getPhoneNumber());
     }
+
+    static Consumer<Customer> greetUsingConsumerFI = (customer) -> System.out.printf("Hi %s, your phone number is %s",
+            customer.getName(), customer.getPhoneNumber());
 
     @Data
     @AllArgsConstructor
